947cfcf39b89d201a6d849fdf895ec8df79fbc72
Evan Busse <evanbusse@gmail.com>
2017-07-30 22:18:31 -0600

    adding image src to lightbox image

--- assets/merch.js
+++ assets/merch.js
@@ -1,11 +1,18 @@
-$('.preview').click(function (e) {
+$('.preview').on('click', function () {
   event.preventDefault();
 
   var shirtID = $(event.target).parents('div.actions')[0].id;
+  var imgSRC = $(event.target).parents('div.merch-item').children('img')[0].src;
+  var $lightbox = $('#lightbox-wrapper')
+  var $pvImg = $('#lightbox-img')
 
-  var imgURL = $(event.target).parents('div.merch-item').children('img')[0].src;
-  console.log(imgURL);
-
+  // Show lightbox
+  $pvImg.attr('src', imgSRC)
+  $lightbox.fadeIn(200)
+  // Hide lightbox on click
+  $lightbox.on('click', function() {
+    $lightbox.fadeOut(200)
+  })
 })
 
 $('.purchase').click(function (e) {
