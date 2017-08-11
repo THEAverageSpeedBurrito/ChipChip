997078a415a6619a1da483a35da40dc9aaa04ad7
Evan Busse <evanbusse@gmail.com>
2017-08-11 08:03:02 -0600

    refactoring

--- assets/merch.js
+++ assets/merch.js
@@ -1,32 +1,52 @@
-$('.preview').on('click', function () {
-  event.preventDefault();
+(function() {
+  var $actionBar = $('#action-bar')
+  var $itemCount = $('#item-count')
 
-  var shirtID = $(event.target).parents('div.actions')[0].id;
-  var imgSRC = $(event.target).parents('div.merch-item').children('img')[0].src;
-  var $lightbox = $('#lightbox-wrapper')
-  var $pvImg = $('#lightbox-img')
+  // check for existing items in the cart;
+  if(!localStorage.getItem('cart')){
+    // initializing cart object
+    var cart = []
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }else{
+    // cart object exists
+    var cart = JSON.parse(localStorage.getItem('cart'))
+    $itemCount.text = cart.length
+  }
 
-  // Show lightbox
-  $pvImg.attr('src', imgSRC)
-  $lightbox.fadeIn(200)
-  // Hide lightbox on click
-  $lightbox.on('click', function() {
-    $lightbox.fadeOut(200)
+
+  $('.preview').on('click', function () {
+    event.preventDefault();
+
+    var shirtID = $(event.target).parents('div.actions')[0].id;
+    var imgSRC = $(event.target).parents('div.merch-item').children('img')[0].src;
+    var $lightbox = $('#lightbox-wrapper')
+    var $pvImg = $('#lightbox-img')
+
+    // Show lightbox
+    $pvImg.attr('src', imgSRC)
+    $lightbox.fadeIn(200)
+    // Hide lightbox on click
+    $lightbox.on('click', function() {
+      $lightbox.fadeOut(200)
+    })
   })
-})
 
-$('.purchase').click(function (e) {
-  event.preventDefault();
+  $('.purchase').click(function (e) {
+    event.preventDefault();
 
-  var shirtID = $(event.target).parents('div')[0].id;
-  var shirtName = event.target.value
-  console.log(shirtName);
+    var shirtID = $(event.target).parents('div')[0].id;
+    var shirtName = event.target.value
+    console.log(shirtName);
 
-  var href = `./purchase.html?shirtid=${shirtName}`
 
-  // page fade out effect
-  $('body').fadeOut(350, function () {
-    window.location.href = href;
+
+    // var href = `./purchase.html?shirtid=${shirtName}`
+
+    // page fade out effect
+    // $('body').fadeOut(350, function () {
+    //   window.location.href = href;
+    // })
+
   })
 
-})
+})()
