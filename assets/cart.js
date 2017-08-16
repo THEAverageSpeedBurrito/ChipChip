b027509c8c5d9c6810fa0cb927fa0dcdbaeb5057
Evan Busse <evanbusse@gmail.com>
2017-08-16 12:14:16 -0600

    can delete items from cart

--- assets/cart.js
+++ assets/cart.js
@@ -4,19 +4,35 @@
   // Getting cart object
   var cart = JSON.parse(localStorage.getItem('cart')) || []
 
-  // loop through and append a new row
-  cart.forEach((item) => {
-    let htmlSnippet = `
-      <tr>
-        <td><img src="${item.image}" alt="shirt img"></td>
-        <td>${item.name}</td>
-        <td>${item.size}</td>
-        <td>$${item.price}</td>
-        <td><img src="assets/icons/trashcan.svg" class="delete" alt="Delete"></td>
-      </tr>
-    `
-    $('#item-list').append(htmlSnippet)
-  })
+  // Render cart
+  renderCart(cart)
+
+
+  function renderCart(cartList) {
+    $('#item-list').empty()
+
+    cartList.forEach((item) => {
+      let htmlSnippet = `
+        <tr>
+          <td><img src="${item.image}" alt="shirt img"></td>
+          <td>${item.name}</td>
+          <td>${item.size}</td>
+          <td>$${item.price}</td>
+          <td><img src="assets/icons/trashcan.svg" class="delete" alt="Delete" value="${cartList.indexOf(item)}"></td>
+        </tr>
+      `
+      $('#item-list').append(htmlSnippet)
+
+    })
+
+    $('.delete').on('click', function() {
+      var index = event.target.getAttribute('value');
+
+      cart.splice(index, 1)
+      renderCart(cart)
+      localStorage.setItem('cart', JSON.stringify(cart))
+    })
+  }
 
 
 }())
