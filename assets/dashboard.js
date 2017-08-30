48c4001b733c9ed757f2993d788d58a932fe7b0a
Evan Busse <evanbusse@gmail.com>
2017-08-30 14:56:28 -0600

    getting order num

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,5 +1,8 @@
 (function() {
   console.log('Welcome to the admin dashboard');
 
-  
+  $('.addshipping').on('click', (e) => {
+    var ordernum = $(event.target).siblings('div.ordernum').text().split(' ')[1];
+    console.log(ordernum);
+  })
 }())
