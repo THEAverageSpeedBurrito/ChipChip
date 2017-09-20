fd0b1ff212a5eaca3027e55ec55c89b91590f5b5
Evan Busse <evanbusse@gmail.com>
2017-09-20 16:11:25 -0600

    checkout page hooked up

--- assets/cart.js
+++ assets/cart.js
@@ -18,10 +18,10 @@
       totalCost += item.price;
       let htmlSnippet = `
         <tr>
-          <td><img src="${item.image}" alt="shirt img"></td>
+          <td><img src="./assets/shirts/${item.imgUrl}.jpg" alt="shirt img"></td>
           <td>${item.name}</td>
           <td>${item.size}</td>
-          <td>$${item.price}</td>
+          <td>$${item.cost}</td>
           <td><img src="assets/icons/trashcan.svg" class="delete" alt="Delete" value="${cartList.indexOf(item)}"></td>
         </tr>
       `
